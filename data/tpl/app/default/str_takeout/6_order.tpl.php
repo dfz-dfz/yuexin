a7a8a3ab0ea16daa50d3603019ee1439ee3b6734
dfz-dfz <1282403099@qq.com>
2019-02-23 18:05:05 +0800

    2.23 project update

--- data/tpl/app/default/str_takeout/6_order.tpl.php
+++ data/tpl/app/default/str_takeout/6_order.tpl.php
@@ -7,6 +7,9 @@
 	.nav a{width:33.333%;}
 	<?php  } ?>
 	body{background-color: #F7F7F7;}
+	.order_fixed .fixed{bottom:0;padding: 0;height: 49px;width: 100%;display: flex;align-items: center;justify-content: space-between;}
+	.order_fixed .fixed span{margin-left: 14px;font-size: 15px;color: #333;font-weight: 400;}
+	.order_fixed .submit_order{width: 119px;height: 49px;margin: 0;line-height: 49px;font-size: 15px;}
 </style>
 <header class="menus">
 	<span></span>
@@ -49,7 +52,7 @@
 			<ul class="menu_list order_list" id="orderList">
 				<div class="order_message">訂單信息</div>
 			<?php  if(is_array($dish_info)) { foreach($dish_info as $li) { ?>
-				<li id="<?php  echo $li['id'];?>">
+				<li id="<?php  echo $li['id'];?>" style="margin-bottom: 10px;">
 					<div class="top">
 						<div class="img_box">
 							<img src="<?php  echo tomedia($li['thumb']);?>" alt="">
@@ -78,44 +81,36 @@
 				</div>
 			</ul>
 			<div class="pay_total">實付  $40</div>
-			<div class="tips" id="addtip" style="background-color:#e1943c;display:none" data-flag="0">亲，您可能还需要以下菜品</div>
-			<!-- 提醒客户需要点的菜品（比如：米饭）-->
-			<?php  if(!empty($dish_add)) { ?>
-				<ul class="menu_list order_list" id="addList" style="display:none">
-				<?php  if(is_array($dish_add)) { foreach($dish_add as $add_li) { ?>
-					<li>
-						<div>
-							<img src="<?php  echo tomedia($add_li['thumb']);?>" alt="">
-						</div>
-						<div>
-							<h3><?php  echo $add_li['title'];?></h3>
-							<div>
-								<div class="fr" max="<?php  echo $add_li['total'];?>">
-									<a href="javascript:void(0);" class="btn_n add active"></a>
-								</div>
-								<input autocomplete="off" class="number" type="hidden" name="dish[<?php  echo $add_li['id'];?>]" value="0">
-								<span class="count">0</span>
-								<strong>$<span class="unit_price"><?php  echo $add_li['member_price'];?></span></strong>
-							</div>
-						</div>
-					</li>
-				<?php  } } ?>
-				</ul>
-			<?php  } ?>
-			<div style="margin:15px 0 0 15px;">
-				<a href="javascript:;" class="comm_btn" style="display: inline-block;background: #51C332;margin-right:15px" id="emptyBtn">重选</a>
+			<div class="link">
+				<div>聯繫方式</div>
+				<div>12345678900</div>
+				<span></span>
+			</div>
+			
+			<div class="pay_type">
+				<div class="pay">
+					<div>支付方式</div>
+					<div>現金支付</div>
+					<span></span>
+				</div>
+				<div class="remarks">
+					<div>備註</div>
+					<div>備註加料、不辣等</div>
+					<span></span>
+				</div>
 			</div>
 		</section>
 		<footer class="order_fixed">
-			<div class="fixed" style="bottom:0;padding: 0;">
-				<a href="<?php  echo $this->createMobileUrl('dish', array('sid' => $sid, 'mode' => $mode, 'tid' => $_GPC['__z']));?>" class="add"><label><span>加菜</span></label></a>
-				<a href="javascript:;" class="comm_btn submit_order" style="display: inline-block;">订单确认</a>
+			<div class="fixed">
+				<span>合計<span class="pay_totals"></span></span>
+				<a href="javascript:;" class="comm_btn submit_order">提交訂單</a>
 			</div>
 		</footer>
 	</form>
 	<?php (!empty($this) && $this instanceof WeModuleSite) ? (include $this->template('footerbar', TEMPLATE_INCLUDEPATH)) : (include template('footerbar', TEMPLATE_INCLUDEPATH));?>
 </div>
-<div class="confirm_box" style="" id="emptyBox">
+
+<!-- <div class="confirm_box" style="" id="emptyBox">
 	<p>确定清空已选菜品吗？</p>
 	<div>
 		<span><a href="javascript:void(0);" class="comm_btn disabled" id="cancel_empty">取消</a></span>
@@ -128,7 +123,7 @@
 		<span><a href="javascript:void(0);" class="comm_btn " id="add_back" style="display: inline-block;background: #51C332;" >去看看</a></span>
 		<span><a href="javascript:void(0);" class="comm_btn" id="add_go">不需要，提交订单</a></span>
 	</div>
-</div>
+</div> -->
 
 <script type="text/javascript">
 	$(function(){
@@ -156,25 +151,33 @@
 		}
 		var prices = parseInt(eval(arr.join('+')))+parseInt($('.lunch_price').text().substring(1))+parseInt($('.dispatching_price').text().substring(1));
 		$('.pay_total').text('實付   $'+prices)
+		$('.pay_totals').text('$'+prices)
+	});
+
+	//支付方式
+	$('.pay').click(function(){
+
 	});
 
 	// 配送類型
 	$('#invite').click(function(){
 		$(this).addClass('ative');
 		$('#takeout').removeClass('ative');
-		$('.invite_box').css('display','block');
-		$('.takeout_box').css('display','none');
-		$('.invite_time').css('display','block');
-		$('.takeout_time').css('display','none');
+		$('.invite_box').show();
+		$('.takeout_box').hide();
+		$('.invite_time').show();
+		$('.takeout_time').hide();
+		$('.link').show();
 	});
 
 	$('#takeout').click(function(){
 		$(this).addClass('ative');
 		$('#invite').removeClass('ative');
-		$('.invite_box').css('display','none');
-		$('.takeout_box').css('display','block');
-		$('.invite_time').css('display','none');
-		$('.takeout_time').css('display','block');
+		$('.invite_box').hide();
+		$('.takeout_box').show();
+		$('.invite_time').hide();
+		$('.takeout_time').show();
+		$('.link').hide();
 	});
 	// 配送類型end
 
@@ -193,23 +196,25 @@
 
 	var is_add = "<?php  echo $is_add;?>";
 	$('.submit_order').click(function(){
-		var add_flag = parseInt($('#addtip').attr('data-flag'));
-		if(!add_flag && is_add == 1) {
-			$('#addBox').dialog({title: '亲，看看我们的推荐菜品嘛！'});
-			$("#add_back").click(function(){
-				$('#addtip').attr('data-flag', 1)
-				$('#addBox').dialog('close');
-				$('#addList, #addtip').slideDown(500);
-				return false;
-			});
-			$("#add_go").click(function(){
-				$('#cart_confirm_form').submit();
-				return false;
-			});
-		} else {
-			$('#cart_confirm_form').submit();
-			return false;
-		}
+		// var add_flag = parseInt($('#addtip').attr('data-flag'));
+		// if(!add_flag && is_add == 1) {
+		// 	$('#addBox').dialog({title: '亲，看看我们的推荐菜品嘛！'});
+		// 	$("#add_back").click(function(){
+		// 		$('#addtip').attr('data-flag', 1)
+		// 		$('#addBox').dialog('close');
+		// 		$('#addList, #addtip').slideDown(500);
+		// 		return false;
+		// 	});
+		// 	$("#add_go").click(function(){
+		// 		$('#cart_confirm_form').submit();
+		// 		return false;
+		// 	});
+		// } else {
+		// 	$('#cart_confirm_form').submit();
+		// 	return false;
+		// }
+		$('#cart_confirm_form').submit();
+		return false;
 	});
 
 	document.addEventListener('WeixinJSBridgeReady', function onBridgeReady() {
